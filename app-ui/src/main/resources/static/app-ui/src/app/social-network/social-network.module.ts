4558aac28e2d6aa3cd31985acbe0a9bc54036e1d
jarek.nowicki <jaroslaw.nowicki@onwelo.com>
2017-12-12 15:24:55 +0100

    next day

---
+++ app-ui/src/main/resources/static/app-ui/src/app/social-network/social-network.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {BrowserModule} from "@angular/platform-browser";
+import {HttpModule} from "@angular/http";
+import {FormsModule} from "@angular/forms";
+import {SocialNetworkRoutingModule} from "./social-network-routing.module";
+import {Ng2KeycloakModule} from "@ebondu/angular2-keycloak";
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    SocialNetworkRoutingModule,
+    Ng2KeycloakModule
+  ],
+  declarations: []
+})
+export class SocialNetworkModule {
+}
